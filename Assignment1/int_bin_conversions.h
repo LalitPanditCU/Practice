bbe8cc2aff5029b59671486f865773c2586897c1
Lalit Pandit <lapa3832@colorado.edu>
2022-03-26 14:16:03 -0600

    Added print_msg. More cleanup.

--- Assignment1/int_bin_conversions.h
+++ Assignment1/int_bin_conversions.h
@@ -10,7 +10,7 @@
 #ifndef INT_BIN_CONVERSIONS_H_
 #define INT_BIN_CONVERSIONS_H_
 
-extern int int_to_binstr(char *str, size_t size, int32_t num, uint8_t nbits);
-extern int uint_to_binstr(char *str, size_t size, uint32_t num, uint8_t nbits);
+int int_to_binstr(char *str, size_t size, int32_t num, uint8_t nbits);
+int uint_to_binstr(char *str, size_t size, uint32_t num, uint8_t nbits);
 
 #endif /* INT_BIN_CONVERSIONS_H_ */
