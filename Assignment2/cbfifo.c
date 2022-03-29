d144f3c62d5301a2447008cfa97367957cd90a10
Lalit <Lalit@Lalit-Home>
2022-03-29 03:17:14 -0600

    Added the function body.

--- Assignment2/cbfifo.c
+++ Assignment2/cbfifo.c
@@ -5,6 +5,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#include "cbfifo.h"
+
 #define CBFIFO_SIZE 128
 const uint32_t cbfifo_mask=(CBFIFO_SIZE-1);
 
@@ -12,6 +14,15 @@ static uint8_t cbfifo[CBFIFO_SIZE];
 static uint32_t capacity=CBFIFO_SIZE;
 static uint32_t read_ptr=0;
 static uint32_t write_ptr=0;
+static int is_full=0;
+
+/*
+ *
+ */
+static size_t cbfifo_vacancies()
+{
+	return capacity-cbfifo_length() ;
+}
 
 /*
  *
@@ -19,8 +30,30 @@ static uint32_t write_ptr=0;
 size_t cbfifo_enqueue(void *buf, size_t nbyte)
 {
 
+    if (is_full)
+    {
+    	return 0;
+    }
 
-	return -1;
+    size_t spaces = cbfifo_vacancies();
+
+    if (spaces < nbyte)
+    {
+    	nbyte = spaces;
+    }
+
+    for (int i = 0; i < nbyte; i++)
+    {
+    	write_ptr = (write_ptr+1) & cbfifo_mask;
+    	*(cbfifo+write_ptr) = *((char *)(buf+i));
+    }
+
+    if (spaces == nbyte)
+    {
+    	is_full = 1;
+    }
+
+	return nbyte;
 }
 
 /*
@@ -28,8 +61,32 @@ size_t cbfifo_enqueue(void *buf, size_t nbyte)
  */
 size_t cbfifo_dequeue(void *buf, size_t nbyte)
 {
-	return -1;
+    if (!is_full)
+    {
+    	return 0;
+    }
+
+    size_t len = cbfifo_length();
+
+    if (len < nbyte)
+    {
+    	nbyte = len;
+    }
+
+    for (int i = 0; i < nbyte; i++)
+    {
+    	*(cbfifo+write_ptr) = *((char *)(buf+i));
+    	write_ptr = (write_ptr+1) & cbfifo_mask;
+    }
+
+    if (len == nbyte)
+    {
+    	is_full = 0;
+    }
+
+	return nbyte;
 }
+
 
 /*
  *
