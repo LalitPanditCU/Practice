f30724396ec7912c79a1d691b712a27ddeb4bb62
Lalit Pandit <lapa3832@colorado.edu>
2022-03-29 22:32:04 -0600

    Finished first implementation.

--- Assignment2/llfifo.c
+++ Assignment2/llfifo.c
@@ -6,6 +6,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "llfifo.h"
 
 typedef struct node_s{
@@ -14,45 +15,67 @@ typedef struct node_s{
 } node_t;
 
 struct llfifo_s{
-	node_t *first;
+	node_t *nodes;
 	node_t *last;
 	uint32_t length;
 	uint32_t capacity;
 };
 
+/*
+ *
+ */
 static node_t *create_node_list(int capacity)
 {
-	node_t *fhd=NULL, *prev=NULL;
+	node_t *fhd=NULL,
+		   *prev=NULL;
 
 	for (int i=0; i < capacity; i++)
 	{
 		node_t *hd = (node_t *)malloc(sizeof(node_t));
+
 		if (hd == NULL)
 		{
 			//FREE THE LIST
 			return NULL;
 		}
 
+		hd->element = NULL;
+		hd->nxt = NULL;
+
 		if (i == 0)
 		{
 			fhd = hd;
-			prev = hd;
 		}
 		else
 		{
-			hd->element = NULL;
-			hd->nxt = NULL;
 			prev->nxt = hd;
 		}
+
+		prev = hd;
 	}
 
 	return fhd;
-
 }
 
+/*
+ *
+ */
+static void destroy_node_list(node_t *nodes)
+{
+	while (nodes)
+	{
+		node_t *prev = nodes;
+		nodes = nodes->nxt;
+
+		free(prev);
+	}
+}
+
+/*
+ *
+ */
 llfifo_t *llfifo_create(int capacity)
 {
-	node_t *nd;
 	llfifo_t *ll;
 
 	ll = (llfifo_t *)malloc(sizeof(struct llfifo_s));
@@ -61,14 +84,22 @@ llfifo_t *llfifo_create(int capacity)
 		return NULL;
 	}
 
-    ll->first = create_node_list(capacity);
-    ll->last = ll->first;
+	ll->nodes = create_node_list(capacity);
+	if (ll->nodes == NULL)
+	{
+		return NULL;
+	}
+
+    ll->last = NULL; //Last occupied
 	ll->length = 0;
 	ll->capacity = capacity;
 
 	return ll;
 }
 
+/*
+ *
+ */
 int llfifo_enqueue(llfifo_t *fifo, void *element)
 {
 	//Parameter check
@@ -78,37 +109,75 @@ int llfifo_enqueue(llfifo_t *fifo, void *element)
 	}
 
 	//Check if the length is less than capacity
-	if (fifo->length < fifo->capacity)
+	if (fifo->length == fifo->capacity)
 	{
-		fifo->last = fifo->last->nxt;
-		fifo->last->element = element;
-		fifo->length += 1;
+		fifo->last->nxt = create_node_list(fifo->capacity); //Add more capacity
+		if (fifo->last->nxt == NULL)
+		{
+			return -1; //Error in memory allocation
+		}
+		fifo->capacity += fifo->capacity;
 	}
-	else
-	{
 
-	}
+	fifo->last = fifo->last->nxt;
+	fifo->last->element = element;
+
+	fifo->length += 1;
+
+	return fifo->length;
 }
 
+/*
+ *
+ */
 void *llfifo_dequeue(llfifo_t *fifo)
 {
+	if (fifo == NULL)
+	{
+		return NULL;
+	}
 
+	if (fifo->length > 0)
+	{
+		node_t *node = fifo->nodes;
+		void *element = node->element;
+
+		fifo->nodes = fifo->nodes->nxt;
+		if (fifo->last == node)
+		{
+			fifo->last = NULL;
+		}
+
+		free (node);
+		fifo->length -= 1;
+
+		return element;
+	}
+
+	return NULL;
 }
 
-
+/*
+ *
+ */
 int llfifo_length(llfifo_t *fifo)
 {
-
+	return fifo->length;
 }
 
-
+/*
+ *
+ */
 int llfifo_capacity(llfifo_t *fifo)
 {
-
+	return fifo->capacity;
 }
 
-
+/*
+ *
+ */
 void llfifo_destroy(llfifo_t *fifo)
 {
-
+	destroy_node_list(fifo->nodes);
+	free(fifo);
 }
