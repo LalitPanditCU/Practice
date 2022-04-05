437b25f7b1db34b116b9a8d3381e62ddc715c2d5
lpandit <03-June-1951>
2022-04-05 11:15:48 -0600

    Added debug stuff to get UART working.

--- Assignment4/source/main.c
+++ Assignment4/source/main.c
@@ -32,13 +32,16 @@
  * @file    Assignment4.c
  * @brief   Application entry point.
  */
+#include <stdio.h>
 #include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
 #include "MKL25Z4.h"
 #include "fsl_debug_console.h"
+/* TODO: insert other include files here. */
 #include "sys_timer.h"
 #include "states.h"
-
-/* TODO: insert other include files here. */
 
 /* TODO: insert other definitions and declarations here. */
 
@@ -48,17 +51,28 @@
 int main(void) {
 
   	/* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
 
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
     /* Init FSL debug console. */
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello world\n\r");
+    PRINTF("Hello World\r\n");
     init_systick();
     init_states();
     state_loop();
 
-
+    /* Force the counter to be placed into memory. */
+    volatile static int i = 0 ;
+    /* Enter an infinite loop, just incrementing a counter. */
+    while(1) {
+        i++ ;
+        /* 'Dummy' NOP to allow source level single stepping of
+            tight while() loop */
+        __asm volatile ("nop");
+    }
     return 0 ;
 }
