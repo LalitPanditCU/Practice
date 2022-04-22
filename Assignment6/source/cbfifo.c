64f68d2096a8c091baa668f6430af0ebb02db045
Lalit Pandit <lapa3832@colorado.edu>
2022-04-22 16:30:41 -0600

    Almost working.

---
+++ Assignment6/source/cbfifo.c
@@ -0,0 +1,180 @@
+/*
+ *
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include "cbfifo.h"
+
+#define CBFIFO_SIZE 128
+const uint32_t cbfifo_mask=(CBFIFO_SIZE-1);
+
+typedef struct {
+	uint8_t cbfifo[CBFIFO_SIZE];
+	uint32_t capacity;
+	uint32_t read_ptr;
+	uint32_t write_ptr;
+	int is_full;
+} cbfifo_t, *cbfifo_ptr_t;
+
+cbfifo_t txcbfifo;
+cbfifo_t rxcbfifo;
+
+cbfifo_t *txcbfifo_ptr = &txcbfifo;
+cbfifo_t *rxcbfifo_ptr = &rxcbfifo;
+
+#define CBFIFO_EMPTY(cb)  ((cb->read_ptr == cb->write_ptr) && (!cb->is_full))
+#define SEL_CBFIFO(x) (((x) == c_RX) ? rxcbfifo_ptr : txcbfifo_ptr)
+
+/*
+ *
+ */
+void cbfifo_init()
+{
+	txcbfifo_ptr->capacity = CBFIFO_SIZE;
+	txcbfifo_ptr->read_ptr = 0;
+	txcbfifo_ptr->write_ptr = 0;
+	txcbfifo_ptr->is_full = 0;
+
+	rxcbfifo_ptr->capacity = CBFIFO_SIZE;
+	rxcbfifo_ptr->read_ptr = 0;
+	rxcbfifo_ptr->write_ptr = 0;
+	rxcbfifo_ptr->is_full = 0;
+}
+
+#ifdef DEBUG
+/*
+ *
+ */
+void _dump_cbfifo(cbfifotype_t xx)
+{
+	cbfifo_ptr_t cb = SEL_CBFIFO(xx);
+
+	size_t len = cbfifo_length(xx);
+	printf("CBFIFO Length: %d\n", len);
+
+	printf("Read_ptr: %d, Write_ptr: %d, Is_full%d\n", cb->read_ptr, cb->write_ptr, cb->is_full);
+
+	uint32_t tmp_ptr = cb->read_ptr;
+	for (int i = 0; i < len; i++)
+	{
+    	char ch = *(cb->cbfifo+tmp_ptr);
+    	printf("%d:%02X ", tmp_ptr, ch);
+    	if (i%16 == 0)
+    	{
+    		printf("\n");
+    	}
+
+    	tmp_ptr = (tmp_ptr+1) & cbfifo_mask;
+	}
+
+	printf("\n");
+
+}
+#endif
+
+/*
+ *
+ */
+static size_t cbfifo_vacancies(cbfifotype_t xx)
+{
+	cbfifo_ptr_t cb = SEL_CBFIFO(xx);
+
+	return cb->capacity-cbfifo_length(xx) ;
+}
+
+/*
+ *
+ */
+size_t cbfifo_enqueue(cbfifotype_t xx, void *buf, size_t nbyte)
+{
+	cbfifo_ptr_t cb = SEL_CBFIFO(xx);
+
+    if (cb->is_full)
+    {
+    	return 0;
+    }
+
+    size_t spaces = cbfifo_vacancies(xx);
+
+    if (spaces < nbyte)
+    {
+    	nbyte = spaces;
+    }
+
+    for (int i = 0; i < nbyte; i++)
+    {
+    	*(cb->cbfifo+cb->write_ptr) = *((char *)(buf+i));
+    	cb->write_ptr = (cb->write_ptr+1) & cbfifo_mask;
+    }
+
+    if (spaces == nbyte)
+    {
+    	cb->is_full = 1;
+    }
+
+	return nbyte;
+}
+
+/*
+ *
+ */
+size_t cbfifo_dequeue(cbfifotype_t xx, void *buf, size_t nbyte)
+{
+	cbfifo_ptr_t cb = SEL_CBFIFO(xx);
+
+    if (CBFIFO_EMPTY(cb)) //Is empty
+    {
+    	return 0;
+    }
+
+    size_t len = cbfifo_length(xx);
+
+    if (len < nbyte)
+    {
+    	nbyte = len;
+    }
+
+    for (int i = 0; i < nbyte; i++)
+    {
+    	*((char *)(buf+i)) = *(cb->cbfifo+cb->read_ptr);
+    	cb->read_ptr = (cb->read_ptr+1) & cbfifo_mask;
+    }
+
+    if (nbyte > 0)
+    {
+    	cb->is_full = 0;
+    }
+
+	return nbyte;
+}
+
+
+/*
+ *
+ */
+size_t cbfifo_length(cbfifotype_t xx)
+{
+	cbfifo_ptr_t cb = SEL_CBFIFO(xx);
+
+	if (cb->is_full)
+	{
+		return cb->capacity;
+	}
+	else
+	{
+		return (cb->write_ptr - cb->read_ptr) & cbfifo_mask;
+
+	}
+}
+/*
+ *
+ */
+size_t cbfifo_capacity(cbfifotype_t xx)
+{
+	return CBFIFO_SIZE;
+}
+
+
--- Assignment6/source/uart.c
+++ Assignment6/source/uart.c
@@ -10,6 +10,7 @@
 
 #include <stdint.h>
 #include "sysclock.h"
+#include "cbfifo.h"
 #include "uart.h"
 
 /*
@@ -18,6 +19,9 @@
  */
 
 #define BAUD_RATE 38400
+
+//#define OVERSAMPLING_RATE (16)
+//#define SBR_VAL  (39)
 
 #define OVERSAMPLING_RATE (16)
 #define SBR_VAL  (39)
@@ -31,6 +35,75 @@
  */
 void UART0_IRQHandler(void)
 {
+	uint8_t ch;
+
+	// Clear the error flag.
+    if (UART0->S1 & (UART_S1_OR_MASK | UART_S1_NF_MASK |
+    		         UART_S1_FE_MASK | UART_S1_PF_MASK))
+    {
+    	UART0->S1 |= UART0_S1_OR_MASK | UART0_S1_NF_MASK  |
+    			     UART0_S1_FE_MASK | UART0_S1_PF_MASK ;
+    	ch = UART0->D;
+    }
+
+    if (UART0->S1 & UART0_S1_RDRF_MASK)
+    {
+    	ch = UART0->D;
+    	if (cbfifo_enqueue(c_RX, (void *)&ch, (size_t)1) <= 0)
+    	{
+    		//Error.
+    	}
+    }
+
+    if ((UART0->C2 & UART0_C2_TIE_MASK)  &&
+	    (UART0->S1 & UART0_S1_TDRE_MASK))
+    {
+    	if (cbfifo_dequeue(c_TX, (void *)&ch, (size_t) 1) == 1)
+    	{
+    		UART0->D = ch;
+    	}
+    	else //Disable the transmit interrupt. No more bytes left in the queue.
+    	{
+    		UART0->C2 &= ~UART0_C2_TIE_MASK;
+    	}
+    }
+}
+
+/*
+ *
+ */
+int __sys_write(int handle, char *buf, int size)
+{
+	/*
+	for (int i=0; i < size;i++)
+	{
+		UART0_Transmit_Poll(*(uint8_t *)(buf+i));
+	}
+	*/
+
+	//Enqueue the data.
+	cbfifo_enqueue(c_TX, (uint8_t *)buf, size);
+
+	//Enable interrupts.
+    UART0->C2 |= UART0_C2_TIE(1);
+
+	return 0;
+}
+
+/*
+ *
+ */
+int __sys_readc(void)
+{
+	uint8_t ch;
+
+   	if (cbfifo_dequeue(c_RX, (void *)&ch, (size_t) 1) == 1)
+   	{
+   		return (int) ch;
+   	}
+
+   	return -1;
+   /*return (int)UART0_Receive_Poll();*/
 }
 
 /*
@@ -65,11 +138,11 @@ void UART0_init()
 	UART0->BDL = UART0_BDL_SBR(sbr);
 	UART0->C4 |= UART0_C4_OSR(OVERSAMPLING_RATE-1);
 
-	// Disable interrupts for RX active edge and LIN break detect, select one stop bit
-	UART0->BDH |= UART0_BDH_RXEDGIE(0) | UART0_BDH_SBNS(0) | UART0_BDH_LBKDIE(0);
+	// Disable interrupts for RX active edge and LIN break detect, select two stop bit
+	UART0->BDH |= UART0_BDH_RXEDGIE(0) | UART0_BDH_SBNS(0) | UART0_BDH_LBKDIE(1);
 
 	// Don't enable loopback mode, use 8 data bit mode, don't use parity
-	UART0->C1 = UART0_C1_LOOPS(0) | UART0_C1_M(0) | UART0_C1_PE(0) | UART_C1_PT(0);
+	UART0->C1 = UART0_C1_LOOPS(0) | UART0_C1_M(0) | UART0_C1_PE(0) ;//| UART_C1_PT(0);
 	// Don't invert transmit data, don't enable interrupts for errors
 	UART0->C3 = UART0_C3_TXINV(0) | UART0_C3_ORIE(0)| UART0_C3_NEIE(0)
 			| UART0_C3_FEIE(0) | UART0_C3_PEIE(0);
@@ -83,6 +156,15 @@ void UART0_init()
 
 	// Send LSB first, do not invert received data
 	UART0->S2 = UART0_S2_MSBF(0) | UART0_S2_RXINV(0);
+
+	// Enable interrupt nvic
+	NVIC_SetPriority (UART0_IRQn, 2); /* set Priority for Systick Interrupt */
+	NVIC_ClearPendingIRQ(UART0_IRQn);
+	NVIC_EnableIRQ(UART0_IRQn);
+
+	// Enable receive interrupts; transmit interrupts enabled when data
+	// is waiting to be transmitted
+    UART0->C2 |= UART0_C2_RIE(1);
 
 	// Enable UART receiver and transmitter
 	UART0->C2 |= UART0_C2_RE(1) | UART0_C2_TE(1);
