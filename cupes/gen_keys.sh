9d3bf219bb709812f4be335e77318852d8ec9ff9
Lalit Pandit <lapa3832@colorado.edu>
2022-05-03 15:52:00 -0600

    Remove the users from sudo privs.

--- cupes/gen_keys.sh
+++ cupes/gen_keys.sh
@@ -2,6 +2,7 @@
 exp="2022-10-30"
 input="names.txt"
 output="pubkeys.txt"
+sudo="sudo.txt"
 dir="files"
 
 zone=:wq
@@ -22,15 +23,24 @@ do
 	echo $var >>$output
 done < "$input"
 
+#Generate sudo remove file
+rm -rf $sudo
+echo "#!/bin/bash" >>$sudo
+while read -r line
+do
+	echo "sudo deluser $line google-sudoers" >>$sudo
+done < "$input"
+
 #Get metadata
 #metadata=$(gcloud compute instances describe $instance) --format("metadata")
 
 #Add ssh keys meta data from describe command to $output
-
 #Set metadata with additional keys
 #gcloud compute instances add-metadata VM_NAME --metadata-from-file ssh-keys=$output
 
 
-#Mail the private ssh keys
+#Mail the private ssh keys from this computer
 #python 0365mail.py
 
+
+#Update startup-script in VMs custom-metadata with the contents of $sudo and restart the VM.
