ed4e4a8f441ecc434cc569d34777fe6bc276d783
lpandit <03-June-1951>
2022-07-11 14:55:53 -0600

    Cleanup

--- Assignment3/source/main.c
+++ Assignment3/source/main.c
@@ -8,6 +8,9 @@
  */
 #include <stdio.h>
 #include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
 #include "MKL25Z4.h"
 #include "fsl_debug_console.h"
 /* Insert other include files here. */
@@ -133,10 +136,17 @@ void run_sequence(colors_t color)
  */
 int main(void) {
 
+  	/* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
     /* Init FSL debug console. */
     BOARD_InitDebugConsole();
 #endif
+
+    PRINTF("Hello World\n\r");
+
 
     init_leds();
     init_touch();
